034b886344d08669b78e2f7dc0114ef8bac167bf
WFaulkner-NO <alighieri01@mail.ru>
2020-05-28 11:58:03 +0300

    3 dopolnitelneh zadaniya

---
+++ Matrix.h
@@ -0,0 +1,66 @@
+#pragma once
+#include <chrono> 
+#include <random>
+#include <iostream>
+
+using namespace std;
+using namespace std::chrono;
+
+template <class T>
+class TMatrix
+{
+protected:
+    int rows, columns;
+    T** matrix;
+
+    void inversion();
+public:
+    TMatrix() :rows(0), columns(0), matrix(0) {};
+    TMatrix(int _r, int _c);
+    TMatrix(const TMatrix& rhs);
+    TMatrix(int _r, int _c, int a, int b);
+    virtual ~TMatrix();
+
+    int get_rows() const;
+    int get_columns() const;
+    T** get_array() const;
+
+    void set_size(int new_r, int new_c);
+
+    const TMatrix<T>& operator =(const TMatrix<T>& rhs);
+
+    T*& operator [](const int i) const;
+
+    template<class P>
+    friend TMatrix<P> operator +(TMatrix<P>& lhs, TMatrix<P>& rhs);
+
+    template <class M>
+    friend TMatrix<M> operator -(TMatrix<M>& lhs, TMatrix<M>& rhs);
+
+    template <class E>
+    friend bool operator ==(TMatrix<E>& lhs, TMatrix<E>& rhs);
+
+    template <class M>
+    friend TMatrix<M> operator *(TMatrix<M>& lhs, TMatrix<M>& rhs);
+
+    template <class M, typename N>
+    friend TMatrix<M> operator *(TMatrix<M>& lhs, N rhs);
+
+    template <class D, typename N>
+    friend TMatrix<D> operator /(TMatrix<D>& lhs, N rhs);
+
+    template <class I>
+    friend istream& operator >>(istream& in, TMatrix<I>& t);
+
+    template <class O>
+    friend ostream& operator <<(ostream& out, const TMatrix<O>& t);
+
+    //Dop zadaniya
+    virtual void Find(T a);
+    virtual void FindAll(T a);
+
+    template <class D>
+    friend TMatrix<D> operator /(TMatrix<D>& lhs, TMatrix<D>& rhs);
+};
+
+#include "Matrix1.h"
---
+++ Vector.h
@@ -0,0 +1,55 @@
+#pragma once
+#include "Matrix.h"
+
+using namespace std::chrono;
+
+template <class T>
+class TVector :public TMatrix<T>
+{
+public:
+	TVector() :TMatrix<T>() {}
+	TVector(int _size) :TMatrix<T>(1, _size) {}
+	TVector(const TVector& t) : TMatrix<T>(t) {}
+	virtual ~TVector();
+	TVector(const std::initializer_list<T>& list);
+	TVector(int n, int a, int b);
+
+	const TVector<T>& operator=(const std::initializer_list<T>& list);
+
+	const TVector<T>& operator =(const TMatrix<T>& rhs);
+
+	template<class P, typename N>
+	friend TVector<N> operator +(TVector<P>& lhs, TVector<N>& rhs);
+
+	template <class M, typename N>
+	friend TVector<N> operator -(TVector<M>& lhs, TVector<N>& rhs);
+
+	template <class D, typename N>
+	friend TVector<D> operator /(TVector<D>& lhs, N rhs);
+
+	template <class M, typename N>
+	friend TVector<M> operator *(TMatrix<M>& lhs, TVector<N>& rhs);
+
+	template <class M, typename N>
+	friend TMatrix<M> operator *(TVector<M>& lhs, TMatrix<N>& rhs);
+
+	T* get_vector();
+	T& operator [](const int i) const;
+
+	template <class I>
+	friend istream& operator >>(istream& in, TVector<I>& t);
+
+	template <class O>
+	friend ostream& operator <<(ostream& out, const TVector<O>& t);
+
+	void BubbleSort();
+	void InsertSort();
+	void QuickSort();
+
+	void Find(T a) override;
+	void FindAll(T a) override;
+protected:
+	void qs(int first, int last);
+};
+
+#include "Vector1.h"
